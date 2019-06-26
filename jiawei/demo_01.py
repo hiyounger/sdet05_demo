027935fc3072f1f908a3969058c983e7faec4e9a
1099415725 <jiaweizhang898@163.com>
2019-06-26 15:05:38 +0800

    practice

---
+++ jiawei/demo_01.py
@@ -0,0 +1,21 @@
+#encoding: utf-8
+week=["星期一","星期二","星期三","星期四"]
+for day in week:
+    print (day)
+
+for i in range(0,15,2):
+    if i == 0:
+        pass
+    else:
+        print(i)
+
+print(range(0,15,2))
+
+while True:
+    try :
+        a=int(input("请输入一个数字："))
+        break
+    except:
+        print("请输入数字")
+
+print(a)
