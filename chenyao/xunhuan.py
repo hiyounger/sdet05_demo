546078c0f248a1b8676fd2be6f2d1a6f35f142af
ychen2019 <947072026@qq.com>
2019-06-26 15:12:38 +0800

    陈耀的4个循环练习

--- chenyao/xunhuan.py
+++ chenyao/xunhuan.py
@@ -7,20 +7,20 @@ while True:
     except:
         print('请输入数字')
 
-# week=['星期一','星期二','星期三','星期四','星期五','星期六','星期天']
-# for w in week:
-#     print(w)
+week=['星期一','星期二','星期三','星期四','星期五','星期六','星期天']
+for w in week:
+    print(w)
 
-# for i in range(0,11):
-#     print(i)
-#
-# for i in range(0,11,2):
-#     if i==0:
-#         pass
-#     else:
-#         print(i)
-#
-# print(range(0,11,2))
+for i in range(0,11):
+    print(i)
+
+for i in range(0,11,2):
+    if i==0:
+        pass
+    else:
+        print(i)
+
+print(range(0,11,2))
 
 
 
