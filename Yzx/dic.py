479c836f4bf981f204dd51b03167d175f2fa4c71
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-06-28 09:08:17 +0800

    猜数字游戏完成,并try catch 和数字100范围

---
+++ Yzx/dic.py
@@ -0,0 +1,10 @@
+#encoding:utf-8
+mermber = [
+    {'id':1,'tel':'18845871680','zhekou':0.8},
+    {'id':2,'tel':'18845095099','zhekou':0.88},
+    {'id':1,'tel':'15165206762','zhekou':0.98}
+]
+tel=input()
+print type(tel)
+for men in mermber:
+    print type(men['tel'])
