1cbc024f77dfe8d861e83a1ceb767ba7595933b4
tttttyj <554748189@qq.com>
2019-07-01 20:45:43 +0800

    0701demo

--- tyj/super_market/model/members.py
+++ tyj/super_market/model/members.py
@@ -1,15 +1,17 @@
 # encoding:utf-8
 from tyj.super_market.db import mysql
+
+
 class get_members():
     @classmethod
     def all_member(cls):
         return mysql.members
 
     @classmethod
-    def get_member_by_tel(cls,tel):
+    def get_member_by_tel(cls, tel):
         get_all_member = []
         for member in mysql.members:
-            if member['tel']==tel:
+            if member['tel'] == tel:
                 get_all_member.append(member)
                 break
             elif member['tel'].endswith(tel):
@@ -17,17 +19,17 @@ class get_members():
         return get_all_member
 
     @classmethod
-    def get_member_by_uid(cls,uid):
+    def get_member_by_uid(cls, uid):
         get_all_member = []
         for member in mysql.members:
-            if member['id']==uid:
+            if member['id'] == uid:
                 get_all_member.append(member)
                 break
         return get_all_member
 
     @classmethod
-    def add_member(cls,tel):
-        new_member={'tel':tel,'disc':1}
-        new_member['uid']=str(len(mysql.members)+1)
+    def add_member(cls, tel):
+        new_member = {'tel': tel, 'disc': 1}
+        new_member['id'] = str(len(mysql.members) + 1)
         mysql.members.append(new_member)
         return new_member
