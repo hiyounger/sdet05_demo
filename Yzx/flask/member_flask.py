51e5a2859c54afc0d6313f11befecb8bc344d14e
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-07-02 13:44:53 +0800

    6个api功能完成实现

--- Yzx/flask/member_flask.py
+++ Yzx/flask/member_flask.py
@@ -13,8 +13,8 @@ def hell():
 @app.route('/json1')
 def json_test():
     json_list = {
-        'return_code':200,
-        'msg':"return member_list successful",
+        'return_code': 200,
+        'msg': "return member_list successful",
         'members': [
             {'id': '1', 'tel': '18845871680', 'disc': 0.9, 'state': 1, 'points': 0000},
             {'id': '2', 'tel': '18845095099', 'disc': 0.1, 'state': 1, 'points': 0000},
@@ -25,33 +25,50 @@ def json_test():
 
 
 @app.route('/members', methods=['GET', 'POST'])
-@app.route('/members/<condition>')
+@app.route('/members/<condition>', methods=['GET', 'PUT','PATCH'])
 def get_all_members(condition=None):
     if request.method == "GET":
         if condition == None:
             get_all_members_list = Mermbers.get_members()
+            # get_all_members_list['msg']='return members  successful'
         else:
             if condition.startswith == 'tel_':
                 tel = condition.split('_')[-1]
                 get_all_members_list = Mermbers.get_members_by_tel(tel)
+                # get_all_members_list['msg'] = 'return members by tel successful'
             else:
                 uid = condition.split('_')[-1]
                 get_all_members_list = Mermbers.get_member_by_uid(uid)
+                # get_all_members_list['msg'] = 'return members by uid successful'
         return jsonify(get_all_members_list)
-    else:
+    elif request.method == "POST":
         tel = request.form['tel']
         new_mem = Mermbers.add_member(tel)
         return jsonify(new_mem)
-
-
-# @app.route('/members')
-# @app.route('/members/<tel>')
-# def get_all_members(tel=None):
-#     if tel==None:
-#         get_all_members_list = Mermbers.get_members()
-#     else:
-#         get_all_members_list=Mermbers.get_members_by_tel(tel)
-#     return str(get_all_members_list)
+    elif request.method == "PUT":
+        uid = condition.split('_')[-1]
+        disc = request.form['disc']
+        tel = request.form['tel']
+        state = request.form['state']
+        user_info = {'disc': disc, 'tel': tel, 'state': state}
+        update_member = Mermbers.update_member_by_id(uid, user_info)
+        if len(update_member) == 0:
+            update_member['return_code'] = 404
+            update_member['return_msg'] = "update member by id false"
+        else:
+            update_member['return_code'] = 202
+            update_member['return_msg'] = "update member by id sucessful"
+        return jsonify(update_member)
+    elif request.method == "PATCH":
+        uid = condition.split('_')[-1]
+        points=request.form['points']
+        return_dic_list=Mermbers.update_member_points(uid,points)
+        return_dic_list['return_code'] = 202
+        return_dic_list['return_msg'] = "update member by id sucessful"
+        return jsonify(return_dic_list)
+    else:
+        return_dic = {'return_msg': '什么都没有做'}
+        return jsonify(return_dic)
 
 
 if __name__ == '__main__':
--- Yzx/flask/model/member.py
+++ Yzx/flask/model/member.py
@@ -5,7 +5,12 @@ from Yzx.flask.db import mysql
 class Mermbers():
     @classmethod
     def get_members(cls):
-        return mysql.members
+        members_dic={
+            'count':len(mysql.members),
+           'members': mysql.members,
+            'msg':"return members successful"
+        }
+        return members_dic
 
     @classmethod
     def get_members_by_tel(cls, tel):
@@ -16,7 +21,11 @@ class Mermbers():
                 break
             elif mem['tel'].endswith(tel):
                 members_list.append(mem)
-        return members_list
+        members_dic={
+            'members':members_list,
+            'msg':"return members by tel sucessful"
+        }
+        return members_dic
 
     @classmethod
     def get_member_by_uid(cls, uid):
@@ -25,7 +34,11 @@ class Mermbers():
             if mem['id']==uid:
                 members_list.append(mem)
                 break
-        return members_list
+        members_dic = {
+            'members': members_list,
+            'msg': "return members by tel sucessful"
+        }
+        return members_dic
 
     @classmethod
     def add_member(cls,tel):
@@ -33,5 +46,30 @@ class Mermbers():
         new_member={'tel':tel,'disc':1.0,'points': 0000,'state': 1}
         new_member['id']=str(len(mysql.members)+1)
         members_list.append(new_member)
-        return str(new_member)
+        return new_member
+
+    @classmethod
+    def update_member_by_id(cls,uid,new_info_dic):
+        for i in range(len(mysql.members)):
+            if mysql.members[i]['id']==uid:
+                for key in new_info_dic.keys():
+                    mysql.members[i][key]=new_info_dic[key]
+                return mysql.members[i]
+        return {}
+
+    @classmethod
+    def update_member_points(cls,uid,points):
+        for i in range(len(mysql.members)):
+            if mysql.members[i]['id']==uid:
+                score_befor=mysql.members[i]['points']
+                score_after=mysql.members[i]['points']+int(points)
+                mysql.members[i]['points']=score_after
+                return_dic={
+                    'uid':uid,
+                    'tel':mysql.members[i]['tel'],
+                    'score_befor':score_befor,
+                    'score_after':score_after,
+                    'score_change':points
+                }
+                return return_dic
 
