5d7e1216f9676ddac5b9d3a211237fc1ef38e437
zhenxingyan <38336539+pingshaluoyan@users.noreply.github.com>
2019-07-02 10:50:57 +0800

    11

--- Yzx/flask/model/member.py
+++ Yzx/flask/model/member.py
@@ -6,3 +6,32 @@ class Mermbers():
     @classmethod
     def get_members(cls):
         return mysql.members
+
+    @classmethod
+    def get_members_by_tel(cls, tel):
+        members_list = []
+        for mem in mysql.members:
+            if tel == mem['tel']:
+                members_list.append(mem)
+                break
+            elif mem['tel'].endswith(tel):
+                members_list.append(mem)
+        return members_list
+
+    @classmethod
+    def get_member_by_uid(cls, uid):
+        members_list = []
+        for mem in mysql.members:
+            if mem['id']==uid:
+                members_list.append(mem)
+                break
+        return members_list
+
+    @classmethod
+    def add_member(cls,tel):
+        members_list=mysql.members
+        new_member={'tel':tel,'disc':1.0,'points': 0000,'state': 1}
+        new_member['id']=str(len(mysql.members)+1)
+        members_list.append(new_member)
+        return str(new_member)
+
