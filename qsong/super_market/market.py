e9c2930d4f6fb2b3731b300d35716a0c1c72508a
qsong <qsong.vip@qq.com>
2019-07-03 17:33:50 +0800

    upload code

--- qsong/super_market/market.py
+++ qsong/super_market/market.py
@@ -1,82 +1,59 @@
 # -*- encoding:utf-8 -*-
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from model.member import db, Member
 
-from qsong.super_market.model.members import Member
-
-app = Flask('__main__')
-
-@app.route('/jsontest')
-def json_test():
-    ret_dic = {
-        'return_code': '200',
-        'msg': 'get member list success',
-        'members':[{'id': '1', 'tel': "18812345672", 'discount': 0.95},
-                   {'id': '2', 'tel': "18812345673", 'discount': 0.9},
-                   {'id': '3', 'tel': "18812345674", 'discount': 0.9},
-                   {'id': '4', 'tel': "18812345671", 'discount': 0.8},
-                   {'id': '5', 'tel': "18811345671", 'discount': 0.8}
-                   ]
-    }
-    return jsonify(ret_dic)
+app = Flask(__name__)
+# 配置数据库连接
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@192.168.8.52:3306/hiMarket"
+db.init_app(app)
 
 
 @app.route('/')
-def say_hello():
+def index():
     return "Hello Flask"
 
 
+@app.route('/initdb', methods=['POST'])
+def init_db():
+    db.create_all()
+    ret_dic = {
+        "return_code":"200",
+        "return_msg":"Init db success"
+    }
+    return jsonify(ret_dic)
 
+@app.route('/member', methods=['POST'])
+@app.route('/member/<condition>', methods=['GET', 'PATCH'])
+def member_actions(condition=None):
+    #1. 处理创建
+    if request.method == 'POST':
+        tel = request.form['tel']
+        mem_info = Member.add_member(tel)
 
-@app.route("/member", methods=['GET', 'POST'])
-@app.route('/member/<condition>', methods=['GET', 'PUT','PATCH'])
-def get_all_members(condition=None):
-    if request.method == 'GET':
-        if condition == None:
-            member_list = Member.get_all_members()
-            member_list['return_code'] = 200
-            member_list['return_msg'] = '获取用户成功'
-        else:
-            if condition.startswith("tel_"):
-                tel = condition.split("_")[-1]
-                member_list = Member.get_members_by_tel(tel)
-                member_list['return_code'] = 200
-                member_list['return_msg'] = 'Get Member by tel success'
-            else:
-                uid = condition.split("_")[-1]
-                member_list = Member.get_members_by_uid(uid)
-                member_list['return_code'] = 200
-                member_list['return_msg'] = 'Get Member by uid success'
-        return jsonify(member_list)
-    elif request.method == 'POST':
-        tel = request.form['tel']
-        new_member = Member.add_member(tel)
-        return jsonify(new_member)
-    elif request.method == 'PUT':
-        uid = condition.split("_")[-1]
-        tel = request.form['tel']
-        discount = request.form['discount']
-        active = request.form['active']
-        user_info = {'tel':tel, 'discount':discount, 'active':active}
-        ret_dic = Member.update_member_info(uid, user_info)
-        if len(ret_dic.keys()) == 0:
-            ret_dic['return_code'] = 404
-            ret_dic['return_msg'] = 'Update user by user info failed'
-        else:
-            ret_dic['return_code'] = 200
-            ret_dic['return_msg'] = 'Update user by user info success'
+        ret_dic = {
+            "return_code":200,
+            "return_msg": "add member success",
+            "member": mem_info
+        }
+
         return jsonify(ret_dic)
+    elif request.method == 'GET':
+        if condition.startswith("tel_"):
+            tel = condition.split("_")[-1]
+            ret_dic = Member.search_by_tel(tel)
+            ret_dic['return_code'] = 200
+            ret_dic['return_msg'] = "Get Member by tel success"
+
+            return jsonify(ret_dic)
     elif request.method == 'PATCH':
-        uid = condition.split("_")[-1]
-        score = request.form['score']
+        uid = int(condition.split("_")[-1])
+        score = int(request.form['score'])
         ret_dic = Member.update_member_score(uid, score)
         ret_dic['return_code'] = 200
-        ret_dic['return_msg'] = 'Update user score success'
+        ret_dic["return_msg"] = "update score success"
         return jsonify(ret_dic)
-    else:
-        ret_dic = {'return_code':'200', 'return_msg':'什么也没做'}
-        return jsonify(ret_dic)
-
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
