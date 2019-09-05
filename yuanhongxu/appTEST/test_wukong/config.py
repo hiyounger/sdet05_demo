7d3b5cb92c13abd2ba96b8950999fb68e8e3cdf4
hongxuY <1944783476@qq.com>
2019-09-05 09:09:00 +0800

    更新

---
+++ yuanhongxu/appTEST/test_wukong/config.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+platformName = "Android"
+platformVersion = "6.0.1"
+deviceName = "816a68b87d74"
+appPackage = "com.kakarote.crm9"
+appActivity = ".MainActivity"
+automationName= "uiautomator2"
+unicodeKeyboard = True
+resetKeyboard = True
+noReset = True
+ip="localhost"
+port=4723
+
+wait_time=0.5
+wait_max_time=20
