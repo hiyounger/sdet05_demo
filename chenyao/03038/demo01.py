d4cc818be1abc4d06ad2e21040452cde6910e8f0
cheernow2022 <947072026@qq.com>
2022-03-08 13:19:00 +0800

    3月8日，13点18分

---
+++ chenyao/03038/demo01.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*—
+
+print range(4)
+print range(0,4)
+print '------'
+print range(0,30,5)
+print range(0,-5,-1)
+print range(0,4,-1)
+
+x = 'hello'
+for i in range(len(x)):
+    print i,x[i]
